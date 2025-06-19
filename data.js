83b2142323a4cf8aaf180aef8ae7c596dfdb8215
owchd <rainxsilent25@gmail.com>
2025-06-19 00:05:04 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,28 +1,26 @@
-// Fake user profiles
 const profiles = {
   flatulunce: {
     username: 'flatulunce',
     bio: 'OG fart artist. Been leaking since birth.',
-    avatar: 'https://cdn.pixabay.com/photo/2015/05/31/11/45/cat-791612_1280.jpg'
+    avatar: 'https://source.unsplash.com/40x40/?cat'
   },
   zuck: {
     username: 'zuck',
     bio: 'Just coded a fart 💻💨',
-    avatar: 'https://cdn.pixabay.com/photo/2017/11/09/21/41/monkey-2939888_1280.jpg'
+    avatar: 'https://source.unsplash.com/40x40/?monkey'
   },
   queenfarter: {
     username: 'queenfarter',
     bio: 'Slay & spray 👑💨',
-    avatar: 'https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_1280.jpg'
+    avatar: 'https://source.unsplash.com/40x40/?dog'
   }
 };
 
-// Starting custom posts
 const posts = [
   {
     id: 1,
     user: 'flatulunce',
-    image: 'https://cdn.pixabay.com/photo/2020/02/06/16/18/surprised-cat-4827021_1280.jpg',
+    image: 'https://source.unsplash.com/600x400/?cat,fart,funny',
     caption: 'Just ripped one on the carpet 💨',
     likes: 55682,
     comments: ['bro 💀', 'this one smelled digital']
@@ -30,7 +28,7 @@ const posts = [
   {
     id: 2,
     user: 'zuck',
-    image: 'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg',
+    image: 'https://source.unsplash.com/600x400/?monkey,tech,fart',
     caption: 'Just coded a fart',
     likes: 120789,
     comments: ['meta methane', 'open source stink']
@@ -38,52 +36,16 @@ const posts = [
   {
     id: 3,
     user: 'queenfarter',
-    image: 'https://cdn.pixabay.com/photo/2017/03/12/13/41/animal-2135875_1280.jpg',
+    image: 'https://source.unsplash.com/600x400/?dog,royalty,fart',
     caption: 'silent but deadly 😳',
     likes: 8420,
     comments: ['bow to the queen', 'she gassed the throne']
   }
 ];
 
-// Working image URLs for random fart posts
-const fartImages = [
-  'https://cdn.pixabay.com/photo/2020/02/06/16/18/surprised-cat-4827021_1280.jpg',
-  'https://cdn.pixabay.com/photo/2015/06/01/01/03/animal-793632_1280.jpg',
-  'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg',
-  'https://cdn.pixabay.com/photo/2017/11/09/21/41/monkey-2939888_1280.jpg',
-  'https://cdn.pixabay.com/photo/2017/03/12/13/41/animal-2135875_1280.jpg',
-  'https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_1280.jpg',
-  'https://cdn.pixabay.com/photo/2015/05/31/11/45/cat-791612_1280.jpg',
-  'https://cdn.pixabay.com/photo/2014/11/17/13/17/cat-534846_1280.jpg',
-  'https://cdn.pixabay.com/photo/2016/02/10/16/37/dog-1194086_1280.jpg',
-  'https://cdn.pixabay.com/photo/2020/01/25/09/51/dog-4791018_1280.jpg'
-];
+const fartKeywords = ['cat', 'dog', 'fart', 'meme', 'stink', 'weird', 'funny', 'gross', 'toilet', 'face'];
 
-// Generate 17 additional posts
 for (let i = 4; i <= 20; i++) {
   const users = Object.keys(profiles);
   const user = users[Math.floor(Math.random() * users.length)];
-  const image = fartImages[Math.floor(Math.random() * fartImages.length)];
-  const captions = [
-    'accidentally crop-dusted 😶‍🌫️',
-    'farted in line at Chipotle',
-    'my chair is now haunted',
-    'I shouldn’t have trusted that one',
-    'blamed it on the cat 🐱💨',
-    'silent... but biblical',
-    'that one had lore',
-    'left something behind 💀',
-    '🔥💨 felt spiritual',
-    'this one had a storyline'
-  ];
-  const caption = captions[Math.floor(Math.random() * captions.length)];
-
-  posts.push({
-    id: i,
-    user,
-    image,
-    caption,
-    likes: Math.floor(Math.random() * 100000),
-    comments: ['🔥🔥', 'that was gnarly']
-  });
-}
+  const randomKeyword = fartKeywo
